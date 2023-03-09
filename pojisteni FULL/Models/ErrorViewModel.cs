53daebcbd15c4c90afccb6d1fd716ff925153c42
DARS68 <dvdrans@gmail.com>
2023-03-09 13:40:24 +0100

    Add project files.

---
+++ pojisteni FULL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pojisteni_FULL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
